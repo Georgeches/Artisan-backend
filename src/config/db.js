9e7369f1847ac4d8a793c82443274433a673a25e
vkhydras <victorkimaru8@gmail.com>
2024-01-17 09:34:17 +0300

    db.js clean up

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://artisans:1234@cluster0.w2v9x1x.mongodb.net/?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://artisans:1234@cluster0.w2v9x1x.mongodb.net/?retryWrites=true&w=majority');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
