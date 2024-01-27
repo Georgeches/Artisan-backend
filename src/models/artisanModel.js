be23780819412d8b6db5610f35fca421c3135185
vkhydras <victorkimaru8@gmail.com>
2024-01-27 10:25:17 +0300

    updated artisan schema

--- src/models/artisanModel.js
+++ src/models/artisanModel.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const artisanSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   phone: String,
   profilePic: String,
   password: { type: String, required: true },
   location: String,
+  town: String,          
+  county: String,        
+  profilepic: String,    
+  coverphoto: String,    
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
 });
